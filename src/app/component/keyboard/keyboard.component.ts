da1563230a1f8a2d0beeb015bdc79db3f3030129
unknown <mletemps@hotmail.com>
2022-06-05 23:46:16 -0400

    initial keyboard layout

--- src/app/component/keyboard/keyboard.component.ts
+++ src/app/component/keyboard/keyboard.component.ts
@@ -3,13 +3,35 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-keyboard',
   templateUrl: './keyboard.component.html',
-  styleUrls: ['./keyboard.component.scss']
+  styleUrls: ['./keyboard.component.scss'],
 })
 export class KeyboardComponent implements OnInit {
+  keyboardArray = [
+    {
+      row: ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+    },
+    {
+      row: ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+    },
+    {
+      row: ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<-BACK'],
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  keyboardHandler(letter: string) {
+    console.log(letter);
   }
 
+  // getKey(val: string): string {
+  //   let returnVal: string;
+  //   if (val === 'ENTER') {
+  //     returnVal = "nice";
+  //   } else {
+  //     returnVal = val;
+  //   }
+  //   return returnVal;
+  // }
 }
