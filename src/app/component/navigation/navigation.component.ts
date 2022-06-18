5aebf7bec6a2769992ba325d9ae45786edda65b4
unknown <mletemps@hotmail.com>
2022-06-18 00:03:07 -0400

    added added ngondestroy

--- src/app/component/navigation/navigation.component.ts
+++ src/app/component/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { EmitterService } from 'src/app/services/emitter.service';
 import { gameNumber } from 'src/app/utilities/interfaces';
@@ -11,13 +12,24 @@ import { gameNumber } from 'src/app/utilities/interfaces';
 export class NavigationComponent implements OnInit {
   // the game/page that is selected
   gameNo: gameNumber = gameNumber.null;
+  // so that gameNumber can be accessible in html
   gameNumberType = gameNumber;
+  // the array of subscriptions
+  subscriptions: Subscription[] = [];
+
   constructor(private emitterService: EmitterService) {
-    this.emitterService.pageNumberCtrlItem$.subscribe((res: gameNumber) => {
-      this.gameNo = res;
-      
-    });
+    this.subscriptions.push(
+      this.emitterService.pageNumberCtrlItem$.subscribe((res: gameNumber) => {
+        this.gameNo = res;
+      })
+    );
   }
 
-  ngOnInit(): void {}
+  ngOnInit() {}
+  ngOnDestroy() {
+    // unsubscribe to subscriptions
+    for (let sub of this.subscriptions) {
+      sub?.unsubscribe();
+    }
+  }
 }
